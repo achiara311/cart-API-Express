9504b0a06c3ef03a9dc3f1bf3507073428c991cb
Anthony Chiara <achiara12@gmail.com>
2020-08-28 14:45:50 -0400

    added git ignore

--- routes.js
+++ routes.js
@@ -3,13 +3,14 @@ const express = require("express");
 
 //creates a new router object
 const routes = express.Router();
+//ROUTES NEED TO GO UNDERNEATH HERE
 
 const cartItems = [
-    { id: 1, product: "apples", price: 1.00, quantity: 10 },
-    { id: 2, product: "oranges", price: 3.00, quantity: 14 },
-    { id: 3, product: "toothpaste", price: 7.00, quantity: 8 },
-    { id: 4, product: "water", price: 4.00, quantity: 16 },
-    { id: 5, product: "wine", price: 20.00, quantity: 10 },
+    { id: 1, product: "apples", price: 1.90, quantity: 10 },
+    { id: 2, product: "oranges", price: 3.70, quantity: 14 },
+    { id: 3, product: "toothpaste", price: 7.50, quantity: 8 },
+    { id: 4, product: "water", price: 4.30, quantity: 16 },
+    { id: 5, product: "wine", price: 20.20, quantity: 10 },
 ];
 let nextId = 6;
 // routes.get("/cart-items", (req, res) => {
@@ -18,19 +19,32 @@ let nextId = 6;
 
 //GET with query param
 routes.get("/cart-items", (req, res) => {
-    const maxPrice = parseInt(req.query.maxPrice);
-
+    const maxPrice = parseFloat(req.query.maxPrice);
+    const prefix = req.query.prefix;
+    const pageSize = req.query.pageSize;
     if (maxPrice <= 3) {
-        const filteredPrices = cartItems.filter(item => item.price <= maxPrice);
+        const filteredPrices = cartItems.filter((item) => item.price <= maxPrice);
         res.status(200);
         res.json(filteredPrices);
+    }
+    else if (prefix) {
+        let filteredPrefix = cartItems.filter((item) => {
+            return item.product.startsWith(prefix);
+            //For Uppercase:
+            // let currentItem = item.product.toLowerCase()
+            // return currentItem.product.startsWith(prefix);
+        });
+        res.json(filteredPrefix);
+    }
+    else if (pageSize) {
+        let myPageSize = parseInt(pageSize);
+        let results = items.slice(0, myPageSize);
+        res.json(results);
     }
     else {
         res.status(200);
         res.json(cartItems);
     }
-    res.status(200);
-    res.json(cartItems);
 });
 
 //GET /cart-items/:id
@@ -38,6 +52,7 @@ routes.get("/cart-items/:id", (req, res) => {
     const id = parseInt(req.params.id);
     const cartItem = cartItems.find(item => item.id === id);
     if (cartItem) {
+        res.status(200);
         res.json(cartItem);
     }
     else {
@@ -63,14 +78,22 @@ routes.put("/cart-items/:id", (req, res) => {
 
     const newCartItem = req.body;
 
-    newCartItem.id = cartId;
+    newCartItem.id = cartId;//saying have the CartId(paramsId) also be the id of what you're editing in the body
     const index = cartItems.findIndex(cartItem => cartItem.id === cartId);//===cartId has to match with const cartId
     cartItems.splice(index, 1, newCartItem);
     res.status(200);
     res.json(newCartItem);
-
-
 });
+
+//PUT ALTERNATIVE
+// routes.put("/cart-items/:id", (req, res) => {
+//     const cartId = parseInt(req.params.id);
+//     let index = cartItems.findIndex((cartItem) => cartItem.id === cartId);
+//     items[index].id=cartId;
+//     cartItems[index]=req.body.id; //KEEP AN EYE ON THIS AND SEE IF IT WORKED ON TEST
+//     res.status(200);
+//     res.json(newCartItem);
+// });
 
 //DELETE /cart-items/:id
 routes.delete("/cart-items/:id", (req, res) => {
@@ -82,6 +105,8 @@ routes.delete("/cart-items/:id", (req, res) => {
     }
     res.status(204);
     res.send("NOT HERE");
+    //OR
+    //res.sendStatus(204);
     //AKA PLAIN TEXT, ON ASSESSMENT
 });
 module.exports = { routes };
--- server.js
+++ server.js
@@ -4,14 +4,18 @@ const express = require("express");
 //require the router object (and all it's defined routes) to be used in this file
 const { routes } = require("./routes");
 
-//creates an Express application - allows us to create and use APIs
+//creates an instance of an Express application - allows us to create and use APIs
 const app = express();
 
+//Allow POST and PUT requests
 //Allow JSON request bodies for PUT and POST
 app.use(express.json());
 
+//use the router object (with all the defined routes)
+//Enable Cross Origin Resource Sharing so this API can be used from web apps on other domains
 //use the router object (and all it's defined routes)
 app.use("/", routes);
+
 
 //define the port
 const port = 3000;
